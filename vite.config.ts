00bde87787a46da05fefe8da68a08d68e320a050
Ascoor <askar@askar.net>
2025-06-26 13:07:42 +0300

    لا

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -10,10 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 7000,
   },
+  base: '/chico-wahtsapp/', // Use repository name as base for GitHub Pages
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
